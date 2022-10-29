b983daef3cebb65062aeb6cb833594d83ac09b06
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-10-29 14:34:16 -0700

    login session works

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const session = require("express-session")
 require("dotenv").config()
 
 // Database Configuration
@@ -9,12 +11,34 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  
+
   // Database Connection Error / Success
 const db = mongoose.connection
 db.on("error", (err) => console.log(err.message + " is mongod not running?"))
 db.on("connected", () => console.log("mongo connected"))
 db.on("disconnected", () => console.log("mongo disconnected"))
+
+// Middleware
+app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }))
+
+// Routes / Controllers
+const userController = require("./controllers/users")
+app.use("/users", userController)
+const sessionsController = require("./controllers/sessions")
+app.use("/sessions", sessionsController)
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+  })
 
 // Listener
 const PORT = process.env.PORT
